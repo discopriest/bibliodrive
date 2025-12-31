5e86f6020b46a7d0b27678ab2762350b4427b967
yuuki <maroua0027@gmail.com>
2025-12-31 17:06:25 +0100

    Add footer.php for common footer structure

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<?php
+// Pied de page commun
+?>
+        </div>
+        <?php include __DIR__ . '/sidebar.php'; ?>
+    </div>
+</main>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
